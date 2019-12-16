59090feadf42f871d1edfff0c0dd351c7dec6589
danny <dannny@joinpad.net>
2019-12-17 00:25:53 +0100

    Adding merge_sort

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <time.h>
 #include <string.h>
 
@@ -7,36 +8,40 @@
 int main(int argc, char * argv[])
 {
 	clock_t t;
+	double time_taken;
 	int array[8];
-	array[0] = 42;
-	array[1] = 38;
-	array[2] = 11;
-	array[3] = 75;
-	array[4] = 99;
-	array[5] = 23;
-	array[6] = 84;
-	array[7] = 67;
+	
+	array[0]=42;
+	array[1]=38;
+	array[2]=11;
+	array[3]=75;
+	array[4]=99;
+	array[5]=23;
+	array[6]=84;
+	array[7]=67;
 	
 	printf("Array to order:\n");
-	print_array(8, array);
+	print_array(8,array);
 	
 	printf("Ordering phases:\n");
-	t = clock();
+	t=clock();
 	
-	if(strcmp(argv[1], "insert")==0)
+	if(strcmp(argv[1],"insert")==0)
 	{
-		insert_sort(8, array);
-	} else if(strcmp(argv[1], "select")==0) {
-		select_sort(8, array);
-	} else if(strcmp(argv[1], "bubble")==0) {
-		bubble_sort(8, array);
+		insert_sort(8,array);
+	} else if(strcmp(argv[1],"select")==0) {
+		select_sort(8,array);
+	} else if(strcmp(argv[1],"bubble")==0) {
+		bubble_sort(8,array);
+	} else if(strcmp(argv[1],"merge")==0) {
+		merge_sort(8,array,0,7);
 	}
 	
-    t = clock() - t;
-    double time_taken = ((double)t)/CLOCKS_PER_SEC;
+    t=clock()-t;
+    time_taken=((double)t)/CLOCKS_PER_SEC;
 
 	printf("Ordered array:\n");
-	print_array(8, array);
+	print_array(8,array);
 	
 	printf("The ordering process took %f seconds to execute.", time_taken);
 	return 0;
@@ -47,8 +52,15 @@ void print_array(int arrc, int arrv[])
 	int i;
 	printf("[ ");
 	for (i=0;i<arrc;i++)
-		printf("%d ", arrv[i]);
+		printf("%d ",arrv[i]);
 	printf("]\n");
+}
+
+void copy_array(int arrv1[], int l1, int r1, int arrv2[], int l2, int r2)
+{
+	int i,j;
+	for (i=l1,j=l2;i<=r1&&j<=r2;i++,j++) 
+		arrv2[j]=arrv1[i];
 }
 
 void insert_sort(int arrc, int arrv[])
@@ -56,11 +68,11 @@ void insert_sort(int arrc, int arrv[])
 	int value,i,j;
 	for(i=0;i<arrc;i++)
 	{
-		for(value=arrv[i],j=i-1;j>=0 && arrv[j]>value;j--)
+		for(value=arrv[i],j=i-1;j>=0&&arrv[j]>value;j--)
 			arrv[j+1]=arrv[j];
 		if(j+1!=i)
 			arrv[j+1]=value;
-		print_array(8, arrv);
+		print_array(8,arrv);
 	}
 }
 
@@ -73,8 +85,8 @@ void select_sort(int arrc, int arrv[])
 		{
 			if(arrv[j]<min)
 			{
-				index = j;
-				min = arrv[index];
+				index=j;
+				min=arrv[index];
 			}
 		}
 		if(index!=i) 
@@ -89,7 +101,7 @@ void select_sort(int arrc, int arrv[])
 void bubble_sort(int arrc, int arrv[])
 {
 	boolean swap;
-	int i, j, tmp;
+	int i,j,tmp;
 	for(swap=True,i=0;i<arrc-1&&swap==True;i++)
 	{
 		for(swap=False,j=arrc-1;j>=i;j--)
@@ -107,3 +119,38 @@ void bubble_sort(int arrc, int arrv[])
 			print_array(8, arrv);
 	}
 }
+
+void merge_sort(int arrc, int arrv[], int left, int right)
+{
+	int length,middle;
+	length=(right-left)+1;
+	if (length>1)
+	{
+		middle=(left+right)/2;
+		merge_sort(length,arrv,left,middle);
+		merge_sort(length,arrv,middle+1,right);
+		merge(length,arrv,left,middle,right);
+		print_array(8,arrv);
+	}
+}
+
+void merge(int arrc, int arrv[], int left, int middle, int right)
+{
+	int i,j,k;
+	int * tmp;
+	tmp=(int *)calloc(arrc, sizeof(int));
+	for(i=left,j=middle+1,k=0;k<arrc;k++)
+	{
+		if((i<=middle&&arrv[i]<=arrv[j])||j>right) 
+		{
+			tmp[k]=arrv[i];
+			i++;
+		} else 
+		{
+			tmp[k]=arrv[j];
+			j++;
+		}
+	}
+	copy_array(tmp,0,arrc,arrv,left,right);
+}
+
--- main.h
+++ main.h
@@ -4,6 +4,11 @@ typedef enum {
 } boolean;
 
 void print_array(int arrc, int arrv[]);
+void copy_array(int arrv1[], int l1, int r1, int arrv2[], int l2, int r2);
+
 void insert_sort(int arrc, int arrv[]);
 void select_sort(int arrc, int arrv[]);
 void bubble_sort(int arrc, int arrv[]);
+
+void merge_sort(int arrc, int arrv[], int left, int right);
+void merge(int arrc, int arrv[], int left, int middle, int right);
